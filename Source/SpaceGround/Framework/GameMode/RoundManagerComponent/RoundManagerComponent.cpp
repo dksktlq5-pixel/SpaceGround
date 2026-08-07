5e1860cf827921042bd844c3950e675141951721
SEOWOOJEONG <unha1@naver.com>
2026-08-07 17:13:12 +0900

    [서우정 add] 라운드 시작과 끝을 관리하는 매니저(게임모드의 컴포넌트)

---
+++ Source/SpaceGround/Framework/GameMode/RoundManagerComponent/RoundManagerComponent.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RoundManagerComponent.h"
+
+#include "SpaceGround/Framework/GameState/SGGameState.h"
+
+URoundManagerComponent::URoundManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	
+}
+
+void URoundManagerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void URoundManagerComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                           FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
+void URoundManagerComponent::StartRound()
+{
+	CurrentRoundIndex++;
+	OnRoundChanged.Broadcast(CurrentRoundIndex); // 구독자들에게 알림
+	
+	ASGGameState* GS = GetWorld()->GetGameState<ASGGameState>();
+	GS->Set_Round_DamageAmounts_Zero();
+	
+}
+
+void URoundManagerComponent::EndRound()
+{
+	// todo : 게임 스테이트의 map을 게임 인스턴스의 map에 더하기
+}
+
---
+++ Source/SpaceGround/Framework/GameMode/RoundManagerComponent/RoundManagerComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RoundManagerComponent.generated.h"
+/*
+ * 라운드 별 차이 반영하는 클래스
+ */
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnRoundChanged, int32 NewRoundindex);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SPACEGROUND_API URoundManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	URoundManagerComponent();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+	void StartRound();
+	void EndRound();
+	
+	FOnRoundChanged OnRoundChanged;
+	
+	int32 CurrentRoundIndex = 0;
+};
