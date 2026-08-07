8f52a3248f74a61b9d52686a3875d47b763860ed
SEOWOOJEONG <unha1@naver.com>
2026-08-07 17:19:36 +0900

    [서우정 add] 게임 전체 속성별 데미지 누적량 기록
    라운드 별 데미지는 라운드가 끝나면 게임인스턴스의 Map에 데미지 누적 후 라운드별 데미지 저장하는 Map의 각 값은 0.f로 초기화.

--- Source/SpaceGround/Framework/GameInstance/SGGameInstance.cpp
+++ Source/SpaceGround/Framework/GameInstance/SGGameInstance.cpp
@@ -1,12 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SGGameInstance.h"
 
-float USGGameInstance::Count_DamageAmount(float Element)
+void USGGameInstance::Init()
 {
-	float New_DamageAmount;
-	New_DamageAmount = Element;
+	Super::Init();
 	
-	return New_DamageAmount;
+	TotalDamageAmounts.FindOrAdd()
+}
+
+void USGGameInstance::Shutdown()
+{
+	Super::Shutdown();
+}
+
+void USGGameInstance::Initialize_TotalDamageAmounts()
+{
+	for (auto& Pair : TotalDamageAmounts)
+	{
+		Pair.Value = 0.f;
+	}
+}
+
+// todo : 라운드 종료 시 호출
+void USGGameInstance::Add_DamageAmount(EDamageElement Element, float DamageAmount)
+{
+	TotalDamageAmounts.Add(Element) += DamageAmount;
 }
--- Source/SpaceGround/Framework/GameInstance/SGGameInstance.h
+++ Source/SpaceGround/Framework/GameInstance/SGGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "SpaceGround/CommonData/SGDamageTypes.h"
 #include "SGGameInstance.generated.h"
 
 /**
@@ -25,25 +26,17 @@ private:
 	virtual void Init() override; // 게임 시작 시 1회 (레벨 로드보다도 먼저)
 	virtual void Shutdown() override; // 게임 종료 시 -> 게임 진행상황 저장?
 	
+	void Initialize_TotalDamageAmounts();
+	
 	// todo : , 옵션 전역 설정, SaveGame 오브젝트 저장 함수
-	
-	// 데미지 통계 변수 및 함수
+#pragma region Damage Statistics
 public:
-	UPROPERTY(Blueprintable)
-	float DamageAmount_FirstElement;
-	
-	UPROPERTY(Blueprintable)
-	float DamageAmount_SecondElement;
-	
-	UPROPERTY(Blueprintable)
-	float DamageAmount_ThirdElement;
-	
-	UPROPERTY(Blueprintable)
-	float DamageAmount_FourthElement;
-	
-	UPROPERTY(Blueprintable)
-	float DamageAmount_FifthElement;
+	UPROPERTY(BlueprintReadOnly)
+	TMap<EDamageElement, float> TotalDamageAmounts; // 모든 라운드 합산 속성별 데미지
 
 	UFUNCTION(BlueprintCallable)
-	float Count_DamageAmount(float Element);
+	void Add_DamageAmount(EDamageElement Element, float DamageAmounts);
+
+#pragma endregion
+	
 };
